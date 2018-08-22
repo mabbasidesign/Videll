4ee2081ebe010ded9c42d7a0a2d4f8f699fdae4f
Mohsen <mabbasidesign@yahoo.com>
2018-08-21 22:41:28 -0600

    add control for username

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 
 class LoginForm extends Component {
 
+    state = {
+        account: {username: "", password: ""},
+    };
+
+    username = React.createRef();
+
+    componentDidMount(){
+        this.username.current.focus();
+    }
+
     handleSubmit = e => {
         e.preventDefault();
         console.log("submitted")
+
+        const username = this.username.current.value;
+    }
+
+    handleChange = e => {
+        const account = {...this.state.account};
+        account.username = e.currentTarget.value;
+        this.setState({ account });
     }
 
     render() { 
@@ -14,7 +32,12 @@ class LoginForm extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="username">Username:</label>
-                        <input id="username" type="text" className="form-control"/>
+                        <input
+                        onChange={this.handleChange}
+                        value={this.state.account.username}
+                        ref={this.username}
+                        id="username" type="text"
+                        className="form-control"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Username:</label>
