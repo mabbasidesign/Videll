8f6d00a11ebd15c13ab74558c0340de24b942f07
Mohsen <mabbasidesign@yahoo.com>
2018-08-24 08:31:13 -0600

    validation on change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -27,26 +27,41 @@ class LoginForm extends Component {
 
         const errors = this.validate();
         console.log(errors);
-        this.setState({ errors });
+        this.setState({ errors: errors || {} });
         if(errors) return;
 
         console.log("submitted");
     }
-
-    handleChange = e => {
-        const account = {...this.state.account};
-        account[e.currentTarget.name] = e.currentTarget.value;
-        this.setState({ account });
+    
+    validateProperty = ({ name, value }) => {
+        if(name === "username"){
+            if(value.trim() === '') return "username is required";
+        }
+            
+        if(name === "password"){
+            if(value.trim() === '') return "password is required";
+        }
     }
 
-    // handleChange = ({ currentTarget: input }) => {
+    // handleChange = e => {
     //     const account = {...this.state.account};
-    //     account[input.name] = input.value;
+    //     account[e.currentTarget.name] = e.currentTarget.value;
     //     this.setState({ account });
     // }
 
+    handleChange = ({ currentTarget: input }) => {
+        const errors = {...this.state.errors}
+        const errorMessage = this.validateProperty(input);
+        if(errorMessage) errors[input.name] = errorMessage;
+            else delete errors[input.name];
+        
+        const account = {...this.state.account};
+        account[input.name] = input.value;
+        this.setState({ account, errors });
+    }
+
     render() {
-        const { account } = this.state;
+        const { account,errors } = this.state;
 
         return ( 
             <div>
@@ -57,12 +72,14 @@ class LoginForm extends Component {
                     value={account.username}
                     label="Username"
                     onChange={this.handleChange}
+                    error={errors.username}
                 />
                  <Input
                     name="password"
                     value={account.password}
                     label="Password"
                     onChange={this.handleChange}
+                    error={errors.password}
                 />
                     <button type="submit" className="btn btn-primary">Login</button>
                 </form>
