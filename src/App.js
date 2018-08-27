18bf0d206dbd6bf67dbb4a050432a3fe71f01b56
Mohsen <mabbasidesign@yahoo.com>
2018-08-27 00:52:25 -0600

    exteract servies to add back-end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Movies from './components/movies'
+import Movies from './components/movies';
+import { ToastContainer } from "react-toastify";
 import Customers from './components/customers';
 import Rental from './components/rental';
 import MovieForm from './components/movieForm';
@@ -7,13 +8,15 @@ import NotFound from './components/notFound';
 import NavBar from "./components/navBar";
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
-import { Route, Redirect, Switch } from 'react-router-dom'
+import { Route, Redirect, Switch } from 'react-router-dom';
+import "react-toastify/dist/ReactToastify.css"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <NavBar />
         <main className="container">
           <Switch>
